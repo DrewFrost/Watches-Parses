314ddfe17df959f04a0a44eec6d93119251789f5
DrewFrost <drew.vfrost@gmail.com>
2019-12-11 16:12:06 +0200

    getting links

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
-  const pageURL = 'https://www.watchfinder.co.uk';
+  const pageURL = 'https://www.watchfinder.co.uk/Tissot/watches';
 
   try {
     await page.goto(pageURL);
     console.log(`Openning: ${pageURL}`);
-  } catch (error) {
-    console.log(`Couldn't open: ${pageURL} cause of: ${error}`);
-  }
+    var links = await page.evaluate(() => {
+      var watchesLinks = document.querySelectorAll(
+        'div > div.col-push-5.col-7.col-md-12.prods_content > a'
+      );
+      var linksResult = [];
+      for (var i = 0; i < 10; i++) {
+        linksResult[i] = watchesLinks[i].getAttribute('href')
+      }
+      return linksResult;  
+    });
 
+    
+  } catch (error) {
+    console.log(`Couldn't proceed: ${pageURL} cause of: ${error}`);
+  }
   await browser.close();
+  console.log(links);
   process.exit();
 })();
